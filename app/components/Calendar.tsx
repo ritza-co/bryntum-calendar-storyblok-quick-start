f37a2c536a3674f0ca186bb524d31a4ad818ed3c
Arsalan Khattak <me@khattak.dev>
2025-07-10 16:15:58 +0500

    refactor

--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -1,15 +1,10 @@
 "use client";
 
 import { BryntumCalendar } from "@bryntum/calendar-react";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 
 export default function Calendar({ ...props }) {
   const calendarRef = useRef<BryntumCalendar>(null);
-
-  useEffect(() => {
-    // Bryntum Calendar instance
-    const calendar = calendarRef?.current?.instance;
-  }, []);
 
   return <BryntumCalendar {...props} ref={calendarRef} />;
 }
